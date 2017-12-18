93cfe206ac7496936620eace86ca0a5d8738f72f
Luke Pfeiffer <lukerpfeiffer@gmail.com>
2017-12-18 18:16:26 -0500

    Fix root path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { ContactComponent }    from './contact/contact.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/about', pathMatch: 'full' },
+  { path: '/', redirectTo: '/about', pathMatch: 'full' },
   { path: 'projects', component: ProjectsComponent },
   { path: 'resume', component: ResumeComponent },
   { path: 'about', component: AboutComponent },
