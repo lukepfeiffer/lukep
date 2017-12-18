0be5ef5f5ab32489757c223a8c04060aead275ef
Luke Pfeiffer <lukerpfeiffer@gmail.com>
2017-12-18 17:11:01 -0500

    Generate contact component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppRoutingModule } from './/app-routing.module';
 import { ProjectsComponent } from './projects/projects.component';
 import { AboutComponent } from './about/about.component';
 import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
@@ -18,7 +19,8 @@ import { ResumeComponent } from './resume/resume.component';
     FooterComponent,
     ProjectsComponent,
     AboutComponent,
-    ResumeComponent
+    ResumeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
